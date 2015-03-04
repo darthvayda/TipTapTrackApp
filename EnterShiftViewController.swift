fbbe8ec126076d64b2ffd71ec7caad41807fe6da
darthvayda <adamhendle@gmail.com>
2015-03-03 23:05:06 -0800

    ttt
    ttt 1st commit

---
+++ EnterShiftViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EnterShiftViewController.swift
+//  TipTapTrackApp
+//
+//  Created by Adam Hendle on 3/3/15.
+//  Copyright (c) 2015 Adam Hendle. All rights reserved.
+//
+
+import UIKit
+
+class EnterShiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
